02c9b237d403a6256f87f374f9ec61b6ccbedcf6
netbiel <netbiel@gmail.com>
2014-07-12 13:24:36 +0200

    Version 1.0.9

--- views/snippets/image_area.php
+++ views/snippets/image_area.php
@@ -6,7 +6,7 @@
 
 	<?php
 
-   $featured_image =  maxflat_get_featured_image( 'wide-image' );
+
 
 	if ('' != get_the_post_thumbnail() ) {
 		?>
@@ -16,11 +16,5 @@
 
 		</div>
 		<?php
-	}else if ( ! empty( $featured_image ) ) {
-		?>
-	<div class="smartlib-single-image-container">
-	  <?php echo $featured_image; ?>
-	</div>
-			<?php
 	}
 	?>
--- views/snippets/video_image_area.php
+++ views/snippets/video_image_area.php
@@ -6,17 +6,10 @@
 
 <?php
 
-$video_link = get_post_meta( $post->ID, '_maxflat_video_link', true );
 
 
-	if ( ! empty( $video_link ) ) {
-		?>
-	<div class="smartlib-video-container">
-		<?php echo $video_link; ?>
-	</div>
-	<?php
-	}else if ('' != get_the_post_thumbnail() ) {
-?>
+if ('' != get_the_post_thumbnail() ) {
+	?>
 <div class="smartlib-single-image-container">
 
 	<?php the_post_thumbnail( 'wide-image' ); ?>
